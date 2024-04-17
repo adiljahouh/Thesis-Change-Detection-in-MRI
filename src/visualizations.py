c03eccb58ff58103585e64f3dbb1024bb8d195c5
adiljahouh <a_jahouh@hotmail.com>
2024-04-17 15:31:51 +0200

    fix 3d plotting; todo check reliability of heatmap

--- src/visualizations.py
+++ src/visualizations.py
@@ -20,7 +20,10 @@ def single_layer_similar_heatmap_visual(output_t0,output_t1,dist_flag):
     similar_dis_map_colorize = cv2.applyColorMap(np.uint8(255 * similar_distance_map_rz.data.cpu().numpy()[0][0]), cv2.COLORMAP_JET)
     return similar_dis_map_colorize
 
-
+## TODO: compute request kirsten
+## TODO: pad instead of resize
+## TODO: nifti output? such that its scrollable
+## TODO:
 def multiple_layer_similar_heatmap_visiual(output_t0, output_t1, dist_flag):
     # Assuming output_t0 and output_t1 are torch tensors of shape (n, c, d, h, w)
     interp = nn.Upsample(size=[512,512, 512], mode='trilinear')
@@ -30,21 +33,17 @@ def multiple_layer_similar_heatmap_visiual(output_t0, output_t1, dist_flag):
     # Reshape to (n, c, d*h*w)
     out_t0_flat = output_t0.view(c, d*h*w).transpose(1, 0)
     out_t1_flat = output_t1.view(c, d*h*w).transpose(1, 0)
-    print(out_t0_flat.shape, out_t1_flat.shape)
     # Compute distance using a predefined function
     distance = various_distance(out_t0_flat, out_t1_flat, dist_flag=dist_flag)
-    print(distance.shape)
     similar_distance_map = distance.view(d,h,w).data.cpu().numpy()
     # print(similar_distance_map.shape)
     similar_distance_map_rz = interp(torch.from_numpy(similar_distance_map).unsqueeze(0).unsqueeze(0))
-    print(similar_distance_map_rz.shape)
-    # print(similar_distance_map_rz)
-    grid = pyvista.ImageData(dimensions=np.array(similar_distance_map_rz.shape)+1)  # Create an empty grid()
-    grid.cell_data["Similarity"] = similar_distance_map_rz.data.cpu().numpy()[0][0]
-
+    scaled_data =  similar_distance_map_rz.data.cpu().numpy()[0][0] *255
+    grid = pyvista.ImageData(dimensions=np.array(scaled_data.shape)+1)  # Create an empty grid()
+    grid.cell_data["Similarity"]= scaled_data.flatten(order="F")
     # Create a plotter object and set the grid as the active scalar field
     plotter = pyvista.Plotter()
-    plotter.add_mesh(grid, scalars="Similarity", cmap="bone")
+    plotter.add_mesh(grid, scalars="Similarity")
     plotter.show_grid()
     plotter.show()
     sleep(10)
