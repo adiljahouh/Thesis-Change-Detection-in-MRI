fix axis in baseline and doing a different normalization on the model feature visualIZATION

src/main.py
@@ -1,10 +1,10 @@
 import torch
 import torch.optim as optim
-from network import SaimeseTwoDim
+from network import SimpleSiamese
 from loss_functions import ConstractiveLoss, ConstractiveThresholdHingeLoss
 from loader import imagePairs, balance_dataset
 import os
-from visualizations import merge_images, generate_roc_curve, single_layer_similar_heatmap_visual
+from visualizations import *
 import argparse
 import cv2
 from torch.utils.data import random_split, DataLoader, Subset
@@ -45,7 +45,7 @@ import numpy as np
 ## skipped CV can reintroduce it later
 
 ## https://medium.com/data-science-in-your-pocket/understanding-siamese-network-with-example-and-codes-e7518fe02612
-def predict(siamese_net, test_loader, threshold=0.3):
+def predict(siamese_net, test_loader, name, threshold=0.3):
     siamese_net.to(device)
     siamese_net.eval()  # Set the model to evaluation mode
     distances_list = []
@@ -87,18 +87,19 @@ def predict(siamese_net, test_loader, threshold=0.3):
                     f"{'sagittal' if batch['index_post'][2][i] != -1 else ''}_"
                     f"{batch['index_post'][2][i].item() if batch['index_post'][2][i] != -1 else ''}.jpg"
                 )
+                baseline = get_baseline(input1[i], input2[i])
                 heatmap, out1_trans, out2_trans = single_layer_similar_heatmap_visual(output1[i], output2[i], 'l2')
                 # Save the heatmap
-                save_dir = os.path.join(os.getcwd(), f'./data/heatmaps/twodim')
+                save_dir = os.path.join(os.getcwd(), f'./results/{name}/heatmaps')
                 os.makedirs(save_dir, exist_ok=True)  # Create the directory if it doesn't exist
                 save_path = f'{save_dir}/{filename}'
                 pre_image = np.rot90(np.squeeze(batch['pre'][i]))
                 post_image = np.rot90(np.squeeze(batch['post'][i]))
-                merge_images(pre_image, post_image, np.rot90(heatmap), save_path)
+                merge_images(pre_image, post_image, np.rot90(heatmap), np.rot90(np.squeeze(baseline)), save_path)
     return distances_list, labels_list
 
 def train(siamese_net, optimizer, criterion, train_loader, val_loader, epochs=100, patience=3, 
-          save_dir='models', model_name='masked.pth', device=torch.device('cuda')):
+          save_dir='./results', model_name='masked.pth', device=torch.device('cuda')):
     siamese_net.to(device)
     print(f"Number of samples in training set: {len(train_loader)}")
     print(f"Number of samples in validation set: {len(val_loader)}")
@@ -152,7 +153,7 @@ def train(siamese_net, optimizer, criterion, train_loader, val_loader, epochs=10
             best_loss = avg_val_loss
             consecutive_no_improvement = 0
             # Save the best model
-            save_path = os.path.join(save_dir, model_name)
+            save_path = os.path.join(save_dir, "model.pth")
             torch.save(siamese_net.state_dict(), save_path)
             print(f'Saved best model to {save_path}')
         else:
@@ -183,7 +184,6 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using device:", device)
-    save_dir = f'./models/{args.model}'
     ##TODO: add root path to args.pars because this wont run on server
     subject_images = imagePairs(proc_preop='./data/processed/preop/BTC-preop', 
                   raw_tumor_dir='./data/raw/preop/BTC-preop/derivatives/tumor_masks',
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     print("Number of dissimilar batches:", len([x for x in subject_images if x['label'] == 0]))
     
     if args.model == 'custom':
-        model_type = SaimeseTwoDim()
+        model_type = SimpleSiamese()
     if args.loss == 'CL':
         criterion = ConstractiveLoss(margin=args.margin, dist_flag=args.dist_flag)
     elif args.loss == 'TCL':
@@ -221,12 +221,14 @@ if __name__ == "__main__":
     #TODO: mtrix calc for evaluation
     model_params =  f'{args.model}_{args.dist_flag}_'\
             f'lr-{args.lr}_marg-{args.margin}_thresh-{args.threshold}_loss-{args.loss}'
+    save_dir = f'./results/{model_params}'
+    os.makedirs(save_dir, exist_ok=True)  # Create the directory if it doesn't exist
     best_loss = train(model_type, optimizer, criterion, train_loader=train_loader, val_loader=val_loader, 
             epochs=args.epochs, patience=args.patience, 
             save_dir=save_dir, model_name=f'{model_params}.pth', device=device)
     # elif args.mode == 'predict':
     #     model_type.load_state_dict(torch.load(f"./models/{args.model_name}"))
     
-    distances, labels = predict(model_type, test_loader, args.margin)
+    distances, labels = predict(model_type, test_loader, model_params, args.margin)
 
-    thresholds = generate_roc_curve(distances, labels, f"./models/{args.model}", model_params)
+    thresholds = generate_roc_curve(distances, labels, f"./results/{model_params}")

src/network.py
@@ -5,9 +5,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SaimeseTwoDim(nn.Module):
+class SimpleSiamese(nn.Module):
     def __init__(self):
-        super(SaimeseTwoDim, self).__init__()
+        super(SimpleSiamese, self).__init__()
         # Define the architecture for the Siamese network
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
@@ -43,3 +43,41 @@ class SaimeseTwoDim(nn.Module):
 
         return output1, output2
 
+
+class SiameseMLO(nn.Module):
+    def __init__(self):
+        super(SiameseMLO, self).__init__()
+        # Define the architecture for the Siamese network
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.dropout = nn.Dropout(p=0.5)
+        # self.fc1 = nn.Linear(131072, 128)  # Adjust input size based on input dimensions
+        self.fc1 = nn.Linear(128 * 32 * 32, 128)  # Output size is 128, adjust if needed
+
+    def forward(self, input1, input2):
+        # Forward pass through the Siamese network
+        output1 = F.relu(self.bn1(self.conv1(input1)))
+        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
+        output1 = F.relu(self.bn2(self.conv2(output1)))
+        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
+        output1 = F.relu(self.bn3(self.conv3(output1)))
+        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
+        # output1 = output1.view(output1.size(0), -1)  # Flatten to (batch_size, 128*32*32)
+        # # output1 = self.dropout(output1)
+        # output1 = self.fc1(output1)
+        
+        output2 = F.relu(self.bn1(self.conv1(input2)))
+        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
+        output2 = F.relu(self.bn2(self.conv2(output2)))
+        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
+        output2 = F.relu(self.bn3(self.conv3(output2)))
+        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
+        # output2 = output2.view(output2.size(0), -1)  # Flatten to (batch_size, 128*32*32)
+        # output2 = self.dropout(output2)
+        # output2 = self.fc1(output2)
+
+        return output1, output2

src/visualizations.py
@@ -6,21 +6,23 @@ import os
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from distance_measures import various_distance
-def merge_images(image1, image2, image3, output_path):
+def merge_images(pre, post, heatmap, baseline, output_path):
     # Create a new figure
-    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    fig, axs = plt.subplots(1, 4, figsize=(12, 4))
 
     # Display each image on a separate subplot
-    axs[0].imshow(image1, cmap="gray")
+    axs[0].imshow(pre, cmap="gray")
     axs[0].axis('off')
-    axs[1].imshow(image2, cmap="gray")
+    axs[1].imshow(post, cmap="gray")
     axs[1].axis('off')
-    im = axs[2].imshow(image3)
+    axs[2].imshow(heatmap, cmap="jet")
     axs[2].axis('off')
+    axs[3].imshow(baseline, cmap="jet")
+    axs[3].axis('off')
     
-    # Create a colorbar
-    cbar = plt.colorbar(im, ax=axs[2], orientation='vertical')
-    cbar.set_label('Similarity Distance (Scaled by 255)')
+    # # Create a colorbar
+    # cbar = plt.colorbar(im, ax=axs[2], orientation='vertical')
+    # cbar.set_label('Similarity Distance (Scaled by 255)')
     
     # Adjust spacing between subplots
     plt.tight_layout()
@@ -28,8 +30,12 @@ def merge_images(image1, image2, image3, output_path):
     # Save the merged image
     plt.savefig(output_path)
     plt.close(fig)
-    
-def generate_roc_curve(distances, labels, save_dir, name):
+
+def get_baseline(pre: torch.Tensor, post: torch.Tensor) -> torch.Tensor:  
+    diff = torch.abs(pre - post)
+    return diff.data.cpu().numpy()
+
+def generate_roc_curve(distances, labels, save_dir):
     # # Invert distances because lower distance indicates more similarity
     # distances = [d.cpu().item() for d in distances]
     # labels = [l.cpu().item() for l in labels]
@@ -46,26 +52,31 @@ def generate_roc_curve(distances, labels, save_dir, name):
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title(f'ROC {name}')
+    plt.title(f'Receiver Operating Characteristic')
     plt.legend(loc="lower right")
 
     # Save the plot to the specified directory
-    plt.savefig(os.path.join(save_dir, f'{name}.png'))
+    plt.savefig(os.path.join(save_dir, f'roc.png'))
     plt.close()  # Close the plot to free up memory
     return thresholds
-def single_layer_similar_heatmap_visual(output_t0,output_t1,dist_flag):
 
-    interp = nn.Upsample(size=[512,512], mode='bilinear')
+def single_layer_similar_heatmap_visual(output_t0: torch.Tensor,output_t1: torch.Tensor,dist_flag: str):
+
+    interp = nn.Upsample(size=[256,256], mode='bilinear')
     c, h, w = output_t0.data.shape
+    print("shape: ", c, h, w)
     out_t0_rz = torch.transpose(output_t0.view(c, h * w), 1, 0)
     out_t1_rz = torch.transpose(output_t1.view(c, h * w), 1, 0)
     distance = various_distance(out_t0_rz,out_t1_rz,dist_flag=dist_flag)
     # print(distance)
     similar_distance_map = distance.view(h,w).data.cpu().numpy()
+    print("similar_distance_map: ", similar_distance_map.shape)
+    ## create a 4 dim torch by adding 2 axis to h,w
+    ## torch upsamle expects b,c,h,w
     similar_distance_map_rz = interp(torch.from_numpy(similar_distance_map[np.newaxis, np.newaxis, :]))
     # print(similar_distance_map_rz)
     similar_dis_map_colorize = cv2.applyColorMap(np.uint8(255 * similar_distance_map_rz.data.cpu().numpy()[0][0]), cv2.COLORMAP_JET)
-    return similar_dis_map_colorize, out_t0_rz, out_t1_rz
+    return similar_dis_map_colorize, similar_distance_map_rz.data.cpu().numpy()[0][0]
 
 def plot_and_save_ndarray(data, save_dir, filename):
     # Create a new figure