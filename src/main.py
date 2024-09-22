finish sanity check, normalization fix in similar feature maps and add augmentation mode base code

src/main.py
@@ -44,7 +44,7 @@ import numpy as np
 ## skipped CV can reintroduce it later
 
 ## https://medium.com/data-science-in-your-pocket/understanding-siamese-network-with-example-and-codes-e7518fe02612
-def predict(siamese_net, test_loader, name, threshold=0.3):
+def predict(siamese_net, test_loader, base_dir, device=torch.device('cuda')):
     siamese_net.to(device)
     siamese_net.eval()  # Set the model to evaluation mode
     distances_list = []
@@ -70,13 +70,13 @@ def predict(siamese_net, test_loader, name, threshold=0.3):
                 dist = distance[i].item()  # Get the distance for the i-th pair
                 distances_list.append(dist)
                 labels_list.append(label)
-                prediction = dist < threshold  # Determine if the pair is similar based on the threshold
+            # Visualize the similarity heatmap                
+                prediction = dist < 5  # Determine if the pair is similar based on the threshold
                 if prediction:
                     print(f"Pair {i} is similar with a distance of: {dist}, label: {label}")
                 else:
                     print(f"Pair {i} is dissimilar with a distance of: {dist}, label: {label}")
 
-            # Visualize the similarity heatmap
                 filename = (
                     f"slice_{batch['pat_id'][i]}_"
                     f"{'axial' if batch['index_post'][0][i] != -1 else ''}_"
@@ -92,7 +92,8 @@ def predict(siamese_net, test_loader, name, threshold=0.3):
                 _, distance_array_nearest = single_layer_similar_heatmap_visual(output1[i], output2[i], dist_flag='l2',
                                                                                          mode='nearest')
                 # Save the heatmap
-                save_dir = os.path.join(os.getcwd(), f'./results/{name}/heatmaps')
+                save_dir = os.path.join(os.getcwd(), f'{base_dir}/heatmaps')
+                print(f"Saving heatmap to {save_dir}")
                 os.makedirs(save_dir, exist_ok=True)  # Create the directory if it doesn't exist
                 save_path = f'{save_dir}/{filename}'
                 pre_image = np.rot90(np.squeeze(batch['pre'][i]))
@@ -190,7 +191,7 @@ if __name__ == "__main__":
     parser.add_argument("--patience", type=int, default=5, help="Patience for early stopping")
     parser.add_argument("--margin", type=float, default=7.0, help="Margin for dissimilar pairs")
     parser.add_argument("--threshold", type=float, default=0.3, help="Threshold for similar pairs, prevents overfit")
-    parser.add_argument("skip", type=int, default=2, help=" Every xth slice to take from the image, if 1 take all. Saves memory")
+    parser.add_argument("--skip", type=int, default=2, help=" Every xth slice to take from the image, if 1 take all. Saves memory")
     
     args = parser.parse_args()
 
@@ -231,14 +232,13 @@ if __name__ == "__main__":
     #TODO: mtrix calc for evaluation
     model_params =  f'{args.model}_{args.dist_flag}_'\
             f'lr-{args.lr}_marg-{args.margin}_thresh-{args.threshold}_loss-{args.loss}'
-    save_dir = f'./results/{model_params}'
+    save_dir = f'./results/{model_params}/train_test'
     os.makedirs(save_dir, exist_ok=True)  # Create the directory if it doesn't exist
     best_loss = train(model_type, optimizer, criterion, train_loader=train_loader, val_loader=val_loader, 
             epochs=args.epochs, patience=args.patience, 
             save_dir=save_dir, device=device)
-    # elif args.mode == 'predict':
-    #     model_type.load_state_dict(torch.load(f"./models/{args.model_name}"))
-    
-    distances, labels = predict(model_type, test_loader, model_params, args.margin)
 
-    thresholds = generate_roc_curve(distances, labels, f"./results/{model_params}")
+    
+    distances, labels = predict(model_type, test_loader, base_dir =save_dir, device=device)
+
+    thresholds = generate_roc_curve(distances, labels, save_dir)

src/network.py
@@ -43,41 +43,152 @@ class SimpleSiamese(nn.Module):
 
         return output1, output2
 
+class deeplab_V2(nn.Module):
+    def __init__(self):
+        super(deeplab_V2, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=64,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64,out_channels=64,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1,ceil_mode=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128,out_channels=128,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1,ceil_mode=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128,out_channels=256,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256,out_channels=256,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256,out_channels=256,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1,ceil_mode=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=256,out_channels=512,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512,out_channels=512,kernel_size=3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1, ceil_mode=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=512,out_channels=512,kernel_size=3,dilation=2,padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, dilation=2, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, dilation=2, padding=2),
+            nn.ReLU(inplace=True),
+        )
+
+        ####### multi-scale contexts #######
+        ####### dialtion = 6 ##########
+        self.fc6_1 = nn.Sequential(
+            nn.Conv2d(in_channels=512,out_channels=1024,kernel_size=3,dilation=6,padding=6),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        self.fc7_1 = nn.Sequential(
+            nn.Conv2d(in_channels=1024,out_channels=1024,kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        ####### dialtion = 12 ##########
+        self.fc6_2 = nn.Sequential(
+            nn.Conv2d(in_channels=512,out_channels=1024,kernel_size=3,dilation=12,padding=12),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        self.fc7_2 = nn.Sequential(
+            nn.Conv2d(in_channels=1024,out_channels=1024,kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        ####### dialtion = 18 ##########
+        self.fc6_3 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, dilation=18, padding=18),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        self.fc7_3 = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        ####### dialtion = 24 ##########
+        self.fc6_4 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, dilation=24, padding=24),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        self.fc7_4 = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5)
+        )
+        self.embedding_layer = nn.Conv2d(in_channels=1024,out_channels=512,kernel_size=1)
+        #self.fc8 = nn.Softmax2d()
+        #self.fc8 = fun.l2normalization(scale=1)
+
+    def forward(self,x):
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        conv3_feature = self.conv3(x)
+        conv4_feature = self.conv4(conv3_feature)
+        conv5_feature = self.conv5(conv4_feature)
+        fc6_1 = self.fc6_1(conv5_feature)
+        fc7_1 = self.fc7_1(fc6_1)
+        fc6_2 = self.fc6_2(conv5_feature)
+        fc7_2 = self.fc7_2(fc6_2)
+        fc6_3 = self.fc6_3(conv5_feature)
+        fc7_3 = self.fc7_3(fc6_3)
+        fc6_4 = self.fc6_4(conv5_feature)
+        fc7_4 = self.fc7_4(fc6_4)
+        fc_feature = fc7_1 + fc7_2 + fc7_3 + fc7_4
+        #conv5_feature = self.fc8(x)
+        #fc7_feature = self.fc8(fc)
+        embedding_feature = self.embedding_layer(fc_feature)
+        #score_final_up = F.upsample_bilinear(score_final,size[2:])
+        #return conv4_feature,conv5_feature,fc_feature,embedding_feature
+        return conv5_feature, fc_feature,embedding_feature
+        #return fc_feature, embedding_feature
+        #return embedding_feature
+
+
+class l2normalization(nn.Module):
+    def __init__(self,scale):
+
+        super(l2normalization, self).__init__()
+        self.scale = scale
+
+    def forward(self, x, dim=1):
+        '''out = scale * x / sqrt(\sum x_i^2)'''
+        #f = x.data.cpu().numpy()
+        #scal = self.scale * x * x.pow(2).sum(dim).clamp(min=1e-12).rsqrt().expand_as(x)
+        #sca = scal.data.cpu().numpy()
+        return self.scale * x * x.pow(2).sum(dim).clamp(min=1e-12).rsqrt().expand_as(x)
 
 class SiameseMLO(nn.Module):
-    def __init__(self):
+    def __init__(self,norm_flag = 'l2'):
         super(SiameseMLO, self).__init__()
-        # Define the architecture for the Siamese network
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.dropout = nn.Dropout(p=0.5)
-        # self.fc1 = nn.Linear(131072, 128)  # Adjust input size based on input dimensions
-        self.fc1 = nn.Linear(128 * 32 * 32, 128)  # Output size is 128, adjust if needed
+        self.CNN = deeplab_V2()
+        if norm_flag == 'l2':
+           self.norm = l2normalization(scale=1)
+        if norm_flag == 'exp':
+            self.norm = nn.Softmax2d()
 
-    def forward(self, input1, input2):
-        # Forward pass through the Siamese network
-        output1 = F.relu(self.bn1(self.conv1(input1)))
-        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
-        output1 = F.relu(self.bn2(self.conv2(output1)))
-        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
-        output1 = F.relu(self.bn3(self.conv3(output1)))
-        output1 = F.max_pool2d(output1, kernel_size=2, stride=2)
-        # output1 = output1.view(output1.size(0), -1)  # Flatten to (batch_size, 128*32*32)
-        # # output1 = self.dropout(output1)
-        # output1 = self.fc1(output1)
-        
-        output2 = F.relu(self.bn1(self.conv1(input2)))
-        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
-        output2 = F.relu(self.bn2(self.conv2(output2)))
-        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
-        output2 = F.relu(self.bn3(self.conv3(output2)))
-        output2 = F.max_pool2d(output2, kernel_size=2, stride=2)
-        # output2 = output2.view(output2.size(0), -1)  # Flatten to (batch_size, 128*32*32)
-        # output2 = self.dropout(output2)
-        # output2 = self.fc1(output2)
+    def forward(self,t0,t1):
 
-        return output1, output2
+        out_t0_conv5,out_t0_fc7,out_t0_embedding = self.CNN(t0)
+        out_t1_conv5,out_t1_fc7,out_t1_embedding = self.CNN(t1)
+        out_t0_conv5_norm,out_t1_conv5_norm = self.norm(out_t0_conv5),self.norm(out_t1_conv5)
+        out_t0_fc7_norm,out_t1_fc7_norm = self.norm(out_t0_fc7),self.norm(out_t1_fc7)
+        out_t0_embedding_norm,out_t1_embedding_norm = self.norm(out_t0_embedding),self.norm(out_t1_embedding)
+        return [out_t0_conv5_norm,out_t1_conv5_norm],[out_t0_fc7_norm,out_t1_fc7_norm],[out_t0_embedding_norm,out_t1_embedding_norm]

src/predictions.py
@@ -1,8 +1,7 @@
 import torch
-import torch.optim as optim
 from network import SimpleSiamese
-from loss_functions import ConstractiveLoss, ConstractiveThresholdHingeLoss
-from loader import balance_dataset, control_pairs
+
+from loader import balance_dataset, control_pairs, subject_patient_pairs
 import os
 from visualizations import *
 import argparse
@@ -20,16 +19,21 @@ if __name__ == "__main__":
     parser.add_argument('--model_path', type=str, help='Path to the model to load', required=True)
     parser.add_argument("--preop_dir", type=str, default='./data/processed/preop/BTC-preop', help=
                         "Path to the directory containing the preprocessed data")
-
+    parser.add_argument("--mode", type=str, choices= ['sanity_check', 'augmented'] ,default='sanity_check', help="Mode of testing, sanity_check or augmented")
     args = parser.parse_args()
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Using device:", device)
-    subject_images = control_pairs(proc_preop=args.preop_dir,
-                  image_ids=['t1_ants_aligned.nii.gz'], skip=4)
-    print(f"Total number of images: {len(subject_images)}")
+    if args.mode == 'sanity_check':
+        subject_images = control_pairs(proc_preop=args.preop_dir,
+                    image_ids=['t1_ants_aligned.nii.gz'], skip=16)
+    elif args.mode == 'augmented':
+        subject_images = subject_patient_pairs(proc_preop=args.preop_dir,
+                    image_ids=['t1_ants_aligned.nii.gz'], skip=16, transform=, tumor_sensitivity=0.18)
+        subject_images = balance_dataset(subject_images)
 
-    subject_images = balance_dataset(subject_images)
-    print(f"Total number of images after balancing: {len(subject_images)}")
+    print(f"Total number of images: {len(subject_images)}")
+    ## No balancing, it is intentionally left unbalanced
     print("Number of similar batches:", len([x for x in subject_images if x['label'] == 1]))
     print("Number of dissimilar batches:", len([x for x in subject_images if x['label'] == 0]))
     
@@ -40,11 +44,12 @@ if __name__ == "__main__":
     
     ## using validation split to avoid overfitting
     test_loader = DataLoader(subject_images, batch_size=16, shuffle=False)
-    model_params =  args.model_path.split("/")[-1]
+    model_params =  args.model_path.split("/")[-3]
+    save_dir = f'./results/{model_params}/{args.mode}'
+    os.makedirs(save_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
-
-    model_type.load_state_dict(torch.load(args.model_dir))
+    model_type.load_state_dict(torch.load(args.model_path))
     
-    distances, labels = predict(model_type, test_loader, f"{model_params}/sanity_check", args.margin)
+    distances, labels = predict(model_type, test_loader, save_dir, device)
 
-    thresholds = generate_roc_curve(distances, labels, f"./results/{model_params}/sanity_check")
+    thresholds = generate_roc_curve(distances, labels, save_dir)

src/visualizations.py
@@ -92,8 +92,15 @@ def single_layer_similar_heatmap_visual(output_t0: torch.Tensor,output_t1: torch
     ## normalize it after to 0 1
     similar_distance_map_rz = interp(torch.from_numpy(similar_distance_map[np.newaxis, np.newaxis, :]))
     normalized_distance_map = normalize_np_array(similar_distance_map_rz.data.cpu().numpy()[0][0])
-    assert normalized_distance_map.max() <= 1.0, f"max: {normalized_distance_map.max()}, {normalized_distance_map}, \n {similar_distance_map_rz.data.cpu().numpy()[0][0]}"
-    assert normalized_distance_map.min() >= 0.0, f"min: {normalized_distance_map.min()}"
+    try:
+        assert normalized_distance_map.max() <= 1.0, f"max: {normalized_distance_map.max()}, {normalized_distance_map}, \n {similar_distance_map_rz.data.cpu().numpy()[0][0]}"
+        assert normalized_distance_map.min() >= 0.0, f"min: {normalized_distance_map.min()}"
+    except AssertionError as e:
+        print(f"AssertionError caught: {e}")
+    # Save the tensor for further debugging
+        save_path = "./debug/similar_distance_map_rz_failed.txt"
+        os.makedirs("./debug", exist_ok=True)
+        np.savetxt(save_path, similar_distance_map)
 
     similar_dis_map_colorize = cv2.applyColorMap(np.uint8(255 * similar_distance_map_rz.data.cpu().numpy()[0][0]), cv2.COLORMAP_JET)
     return similar_dis_map_colorize, normalized_distance_map