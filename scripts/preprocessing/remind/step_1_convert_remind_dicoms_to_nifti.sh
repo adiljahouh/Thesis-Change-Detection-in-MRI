1cfc36e009ad9cba3d23fff72eb0b28789cfd5ad
adiljahouh <a_jahouh@hotmail.com>
2024-11-04 16:18:27 +0100

    change bash script for conversion to make it check for files

--- scripts/preprocessing/remind/step_1_convert_remind_dicoms_to_nifti.sh
+++ scripts/preprocessing/remind/step_1_convert_remind_dicoms_to_nifti.sh
@@ -28,26 +28,44 @@ for patient_dir in "$BASE_DIR"/*; do
                 tumor_seg_match=$(find "$preop_dir" -type d | grep -i "$KEYWORD_TUMOR_SEG" | grep -i "$KEYWORD_REF" | grep -i "$KEYWORD_T1")
 
                 if [ -n "$tumor_seg_match" ]; then
-                    # echo "all matches found for $(basename "$patient_dir")"
-                    # echo "preop_match: $preop_match"
-                    # echo "intraop_match: $intraop_match"
-                    # echo "tumor_seg_match: $tumor_seg_match"
+                    echo "all matches found for $(basename "$patient_dir")"
                     
                     counter=$((counter+1))
-                    echo cleaning directories...
-                    rm -rf "$preop_dir/T1_converted"
-                    rm -rf "$intraop_dir/T1_converted"
-                    rm -rf "$preop_dir/tumor_converted"
-                    
-                    mkdir -p "$preop_dir/T1_converted"
-                    mkdir -p "$intraop_dir/T1_converted"
-                    mkdir -p "$preop_dir/tumor_converted"
-                    echo converting to NiFTI...
-                    
-                    dcm2niix -z y  -o "$preop_dir/T1_converted" "$preop_match"
-                    dcm2niix -z y  -o "$intraop_dir/T1_converted" "$intraop_match"
-                    segimage2itkimage --outputDirectory "$preop_dir/tumor_converted" --inputDICOM "$tumor_seg_match/1-1.dcm" -t  nii
 
+                    # Check and create directories if they do not exist
+                    [ ! -d "$preop_dir/T1_converted" ] && mkdir -p "$preop_dir/T1_converted"
+                    [ ! -d "$intraop_dir/T1_converted" ] && mkdir -p "$intraop_dir/T1_converted"
+                    [ ! -d "$preop_dir/tumor_converted" ] && mkdir -p "$preop_dir/tumor_converted"
+
+                    # echo "Converting to NiFTI..."
+
+                    # Check if a .nii.gz file with 'postcontrast' exists in the directory
+                    preop_nifti_exists=$(find "$preop_dir/T1_converted" -type f -iname "*postcontrast*.nii.gz" | wc -l)
+                    intraop_nifti_exists=$(find "$intraop_dir/T1_converted" -type f -iname "*postcontrast*.nii.gz" | wc -l)
+
+                    if [ "$preop_nifti_exists" -gt 0 ]; then
+                        echo "Skipping conversion for patient $(basename "$preop_dir") (preop)"
+                    else
+                        preop_nifti="$preop_dir/T1_converted/$(basename "$preop_match" .dcm).nii.gz"
+                        echo "Converting $preop_match to $preop_nifti"
+                        dcm2niix -z y -o "$preop_dir/T1_converted" "$preop_match"
+                    fi
+
+                    if [ "$intraop_nifti_exists" -gt 0 ]; then
+                        echo "Skipping conversion for patient $(basename "$intraop_dir") (intraop)"
+                    else
+                        intraop_nifti="$intraop_dir/T1_converted/$(basename "$intraop_match" .dcm).nii.gz"
+                        echo "Converting $intraop_match to $intraop_nifti"
+                        dcm2niix -z y -o "$intraop_dir/T1_converted" "$intraop_match"
+                    fi
+
+                    tumor_nifti="$preop_dir/tumor_converted/1.nii.gz"
+                    if [ ! -f "$tumor_nifti" ]; then
+                        echo "Converting tumor.."
+                        segimage2itkimage --outputDirectory "$preop_dir/tumor_converted" --inputDICOM "$tumor_seg_match/1-1.dcm" -t nii
+                    else
+                        echo "tumor already exists, skipping conversion."
+                    fi
                 fi
             fi
 
