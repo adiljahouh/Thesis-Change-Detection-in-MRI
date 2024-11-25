bbe62db256610759c0e75cdf0b32591afe2efd2a
adiljahouh <a_jahouh@hotmail.com>
2024-11-25 15:45:40 +0100

    add upsampled siamese ext for testing

--- src/network.py
+++ src/network.py
@@ -172,10 +172,9 @@ class complexSiameseExt(nn.Module):
 
         return [output1_pool4, output2_pool4], [output1_pool5, output2_pool5], [output1_pool6, output2_pool6]
 
-class complexSiameseNoPool(nn.Module):
+class SiameseExtUpsampled(nn.Module):
     def __init__(self):
-        super(complexSiameseNoPool, self).__init__()
-        # Define the architecture for the Siamese network
+        super(SiameseExtUpsampled, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)        
         self.bn1 = nn.BatchNorm2d(32)
         
@@ -186,40 +185,60 @@ class complexSiameseNoPool(nn.Module):
         self.bn3 = nn.BatchNorm2d(128)
         
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        # Same output channels, just to refine the features later on
+        ## same output channels, just to refine the features later on
         self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(256)
         
         self.conv6 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
         self.conv7 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+    
         self.bn5 = nn.BatchNorm2d(512)
+        
+        self.conv8 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.conv9 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1)
+        self.bn6 = nn.BatchNorm2d(1024)
+        
+        # self.fc1 = nn.Linear(131072, 128)  # Adjust input size based on input dimensions
+        # self.fc1 = nn.Linear(128 * 32 * 32, 128)  # Output size is 128, adjust if needed
 
     def forward(self, input1, input2):
-        # Forward pass through the Siamese network for input1
+        # Forward pass through the Siamese network
         output1_conv1 = F.relu(self.bn1(self.conv1(input1)))
         output1_pool1 = F.max_pool2d(output1_conv1, kernel_size=2, stride=2)
         output1_conv2 = F.relu(self.bn2(self.conv2(output1_pool1)))
-        output1_conv3 = F.relu(self.bn3(self.conv3(output1_conv2)))
-        output1_pool2 = F.max_pool2d(output1_conv3, kernel_size=2, stride=2)
-        output1_conv4 = F.relu(self.conv4(output1_pool2))
+        output1_pool2 = F.max_pool2d(output1_conv2, kernel_size=2, stride=2)
+        output1_conv3 = F.relu(self.bn3(self.conv3(output1_pool2)))
+        output1_pool3 = F.max_pool2d(output1_conv3, kernel_size=2, stride=2)
+        
+        output1_conv4 = F.relu(self.conv4(output1_pool3))
         output1_conv5 = F.relu(self.bn4(self.conv5(output1_conv4)))
-        output1_pool3 = F.max_pool2d(output1_conv5, kernel_size=2, stride=2)
-        output1_conv6 = F.relu(self.conv6(output1_pool3))
-        output1_conv7 = F.relu(self.conv7(output1_conv6))
-
-        # Forward pass through the Siamese network for input2
+        output1_pool4 = F.max_pool2d(output1_conv5, kernel_size=2, stride=2)
+        
+        output1_conv6 = F.relu(self.conv6(output1_pool4))
+        output1_conv7 = F.relu(self.bn5(self.conv7(output1_conv6)))
+        output1_pool5 = F.max_pool2d(output1_conv7, kernel_size=2, stride=2)
+        output1_conv8 = F.relu(self.conv8(output1_pool5))
+        output1_conv9 = F.relu(self.bn6(self.conv9(output1_conv8)))        
+        #######
+        
         output2_conv1 = F.relu(self.bn1(self.conv1(input2)))
         output2_pool1 = F.max_pool2d(output2_conv1, kernel_size=2, stride=2)
         output2_conv2 = F.relu(self.bn2(self.conv2(output2_pool1)))
-        output2_conv3 = F.relu(self.bn3(self.conv3(output2_conv2)))
-        output2_pool2 = F.max_pool2d(output2_conv3, kernel_size=2, stride=2)
-        output2_conv4 = F.relu(self.conv4(output2_pool2))
+        output2_pool2 = F.max_pool2d(output2_conv2, kernel_size=2, stride=2)
+        output2_conv3 = F.relu(self.bn3(self.conv3(output2_pool2)))
+        output2_pool3 = F.max_pool2d(output2_conv3, kernel_size=2, stride=2)
+        
+        output2_conv4 = F.relu(self.conv4(output2_pool3))
         output2_conv5 = F.relu(self.bn4(self.conv5(output2_conv4)))
-        output2_pool3 = F.max_pool2d(output2_conv5, kernel_size=2, stride=2)
-        output2_conv6 = F.relu(self.conv6(output2_pool3))
-        output2_conv7 = F.relu(self.conv7(output2_conv6))
+        output2_pool4 = F.max_pool2d(output2_conv5, kernel_size=2, stride=2)
+    
+        output2_conv6 = F.relu(self.conv6(output2_pool4))
+        output2_conv7 = F.relu(self.bn5(self.conv7(output2_conv6)))
+        output2_pool5 = F.max_pool2d(output2_conv7, kernel_size=2, stride=2)
+        output2_conv8 = F.relu(self.conv8(output2_pool5))
+        output2_conv9 = F.relu(self.bn6(self.conv9(output2_conv8)))
 
-        return [output1_conv3, output2_conv3], [output1_conv5, output2_conv5], [output1_conv7, output2_conv7]
+        return [output1_pool4, output2_pool4], [output1_pool5, output2_pool5], [output1_conv9, output2_conv9]
 
 class l2normalization(nn.Module):
     def __init__(self,scale):
